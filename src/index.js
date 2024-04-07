0db14d398e5d7a814480fda82b2336c5ec890042
ramandagar <ramandagar479@gmail.com>
2024-04-07 15:11:28 +0530

    Next MIcroservice

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+const { PORT } = require('./config/ServerConfig')
+
+const db = require('./models/index')
+
+const apiRoutes = require('./routes/index')
+
+const setUpAndStartServer=()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes)
+
+    app.listen(PORT,()=>{
+        console.log('server started on port')
+    })
+
+    if(process.env.DB_SYNC){
+        db.sequelize.sync({alter:true})
+    }
+}
+
+setUpAndStartServer();
