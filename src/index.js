c3033ebb841824b95528f153da29531d494424dc
ramandagar <ramandagar479@gmail.com>
2024-04-08 23:29:37 +0530

    Added new table

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ const db = require('./models/index')
 const apiRoutes = require('./routes/index')
 
 const setUpAndStartServer=()=>{
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',apiRoutes)
-
     app.listen(PORT,()=>{
         console.log('server started on port')
     })
--- src/models/booking.js
+++ src/models/booking.js
@@ -27,7 +27,17 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       values:['In Process','Booked','Cancel'],
       defaultValue: 'In Process'
-    }
+    },
+    noOfSeats: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    }, 
+    totalCost: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
   }, {
     sequelize,
     modelName: 'Booking',
